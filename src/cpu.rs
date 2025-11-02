e1be41c101445776b852cb89eb4e517cd62f883c
GP2015 <adamcunningham2015@hotmail.com>
2025-11-02 12:58:28 +0000

    Matched half of opcodes to instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,5 +1,6 @@
-use crate::mapper::UseAsMapper;
-use anyhow::Result;
+mod instructions;
+
+use crate::bus::Bus;
 
 const PC_RESET_ADDR_LOW_BYTE: usize = 0xFFFC;
 const PC_RESET_ADDR_HIGH_BYTE: usize = 0xFFFD;
@@ -11,7 +12,18 @@ const Y_REGISTER_RESET_VALUE: u8 = 0x00;
 const STATUS_REGISTER_RESET_VALUE: u8 = 0x00;
 const STACK_POINTER_RESET_VALUE: u8 = 0xFD;
 
+
+
+type BasicOperationFnPtr = fn(cpu: &mut CPU, address_bus: &mut Bus, data_bus: &mut Bus);
+
+enum BasicOperation{
+    Nothing(BasicOperationFnPtr),
+    ReadMemory(BasicOperationFnPtr, usize),
+    WriteMemory(BasicOperationFnPtr, usize),
+}
+
 pub struct CPU {
+    operations_to_do: Vec<Operation>,
     program_counter: u16,
     accumulator: u8,
     x_register: u8,
@@ -22,7 +34,12 @@ pub struct CPU {
 
 impl CPU {
     pub fn new() -> Self {
+        let mut operations_to_do: vec![];
+
+        operations_to_do[6] = ;
+
         Self {
+            operations_to_do: [do_nothing; MAX_OPERATIONS_TO_DO],
             program_counter: PROGRAM_COUNTER_RESET_VALUE,
             accumulator: ACCUMULATOR_RESET_VALUE,
             x_register: X_REGISTER_RESET_VALUE,
@@ -31,4 +48,16 @@ impl CPU {
             stack_pointer: STACK_POINTER_RESET_VALUE,
         }
     }
+
+    pub fn tick(&mut self, address_bus: &mut Bus, data_bus: &mut Bus) {}
+
+    fn schedule_operations(&mut self, mut operations: Vec<OperationFnPtr>) {
+        self.operations_to_do_ptr = operations.len();
+        operations.reverse();
+        self.operations_to_do[0..operations.len()].copy_from_slice(&operations);
+    }
+}
+
+fn do_nothing(_: &mut CPU, _: &mut Bus, _: &mut Bus) {
+    return;
 }
