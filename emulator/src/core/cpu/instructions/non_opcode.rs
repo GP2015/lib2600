9146b0a25661fa9f7e9caef230aa51d1a90cc9d4
GP2015 <adamcunningham2015@hotmail.com>
2025-11-10 14:05:28 +0000

    Fixed non_opcode program counter control

--- emulator/src/core/cpu/instructions/non_opcode.rs
+++ emulator/src/core/cpu/instructions/non_opcode.rs
@@ -1,4 +1,6 @@
-use super::*;
+use crate::core::bus::Bus;
+use crate::core::cpu::CPU;
+use crate::core::cpu::instructions;
 
 const PC_RESET_ADDR_LOW_BYTE: usize = 0xfffc;
 const PC_RESET_ADDR_HIGH_BYTE: usize = 0xfffd;
@@ -41,14 +43,18 @@ pub fn fetch_rising_edge(cpu: &mut CPU, address_bus: &mut Bus) {
 pub fn fetch_falling_edge(cpu: &mut CPU, data_bus: &mut Bus) {
     let opcode = data_bus.get_combined() as u8;
     let (instruction, addressing_mode) = instructions::fetch_instruction(opcode);
+
     cpu.current_instruction = instruction;
     cpu.current_addressing_mode = addressing_mode;
+
+    cpu.increment_program_counter();
     cpu.reset_cycle_counters();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::core::cpu::instructions::{AddressingMode, Instruction};
 
     fn create_valid_objects() -> (Bus, Bus, CPU) {
         let address_bus = Bus::new(13);
@@ -62,17 +68,17 @@ mod tests {
         let (mut address_bus, mut data_bus, mut cpu) = create_valid_objects();
 
         for _ in 0..5 {
-            cpu.tick_rising_edge(&mut address_bus, &mut data_bus);
-            cpu.tick_falling_edge(&mut address_bus, &mut data_bus);
+            cpu.tick_rising_edge(&mut address_bus);
+            cpu.tick_falling_edge(&mut data_bus);
         }
 
-        cpu.tick_rising_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_rising_edge(&mut address_bus);
         assert_eq!(address_bus.get_combined(), 0x1ffc);
-        cpu.tick_falling_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_falling_edge(&mut data_bus);
 
-        cpu.tick_rising_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_rising_edge(&mut address_bus);
         assert_eq!(address_bus.get_combined(), 0x1ffd);
-        cpu.tick_falling_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_falling_edge(&mut data_bus);
 
         assert_eq!(cpu.current_instruction, Instruction::Fetch);
     }
@@ -84,13 +90,14 @@ mod tests {
         cpu.current_instruction = Instruction::Fetch;
         cpu.current_addressing_mode = AddressingMode::A;
 
-        cpu.tick_rising_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_rising_edge(&mut address_bus);
         assert_eq!(address_bus.get_combined(), 0x67);
 
         data_bus.set_combined(0xea);
 
-        cpu.tick_falling_edge(&mut address_bus, &mut data_bus);
+        cpu.tick_falling_edge(&mut data_bus);
         assert_eq!(cpu.current_instruction, Instruction::NOP);
         assert_eq!(cpu.current_addressing_mode, AddressingMode::Impl);
+        assert_eq!(cpu.program_counter, 0x68);
     }
 }
