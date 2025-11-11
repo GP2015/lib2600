9e78c5ba56ab4b42f2915ec682009180234f1f79
GP2015 <adamcunningham2015@hotmail.com>
2025-11-11 12:06:15 +0000

    Added negative and zero flag

--- emulator/src/core/cpu.rs
+++ emulator/src/core/cpu.rs
@@ -10,6 +10,14 @@ const Y_REGISTER_RESET_VALUE: u8 = 0x00;
 const STATUS_REGISTER_RESET_VALUE: u8 = 0x00;
 const STACK_POINTER_RESET_VALUE: u8 = 0xfd;
 
+const NEGATIVE_FLAG_BIT: u8 = 7;
+const OVERFLOW_FLAG_BIT: u8 = 6;
+const BREAK_FLAG_BIT: u8 = 4;
+const DECIMAL_FLAG_BIT: u8 = 3;
+const INTERRUPT_FLAG_BIT: u8 = 2;
+const ZERO_FLAG_BIT: u8 = 1;
+const CARRY_FLAG_BIT: u8 = 0;
+
 pub struct CPU {
     program_counter: u16,
     accumulator: u8,
@@ -87,5 +95,33 @@ impl CPU {
         self.instruction_cycle = 0;
         self.addressing_cycle = 0;
         self.finished_addressing = false;
+    }
+
+    fn set_status_line(&mut self, line: u8, state: bool) {
+        self.status_register = match state {
+            true => self.status_register | (1 << line),
+            false => self.status_register & !(1 << line),
+        };
+    }
+
+    fn get_status_line(&self, line: u8) -> bool {
+        (self.status_register >> line) & 1 == 1
+    }
+
+    fn set_negative_flag_from_byte(&mut self, byte: u8) {
+        let is_negative = (byte >> 7) == 1;
+        self.set_status_line(NEGATIVE_FLAG_BIT, is_negative);
+    }
+
+    fn get_negative_flag(&self) -> bool {
+        self.get_status_line(NEGATIVE_FLAG_BIT)
+    }
+
+    fn set_zero_flag_from_byte(&mut self, byte: u8) {
+        self.set_status_line(NEGATIVE_FLAG_BIT, byte == 0);
+    }
+
+    fn get_zero_flag(&self) -> bool {
+        self.get_status_line(ZERO_FLAG_BIT)
     }
 }
--- emulator/src/core/cpu/instructions/transfer.rs
+++ emulator/src/core/cpu/instructions/transfer.rs
@@ -9,8 +9,40 @@ pub fn lda_rising(cpu: &mut CPU, address_bus: &mut Bus, rw_line: &mut bool) {
 pub fn lda_falling(cpu: &mut CPU, data_bus: &mut Bus) {
     if cpu.finished_addressing {
         cpu.accumulator = data_bus.get_combined() as u8;
+        cpu.set_zero_flag_from_byte(cpu.accumulator);
+        cpu.set_negative_flag_from_byte(cpu.accumulator);
         cpu.end_instruction();
     } else {
         instructions::execute_addressing_falling(cpu, data_bus);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::core::cpu::instructions::{AddressingMode, Instruction};
+
+    fn create_valid_objects() -> (CPU, Bus, Bus, bool) {
+        let cpu = CPU::new();
+        let address_bus = Bus::new(13);
+        let data_bus = Bus::new(8);
+        let rw_line = false;
+        (cpu, address_bus, data_bus, rw_line)
+    }
+
+    #[test]
+    fn lda() {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_valid_objects();
+        cpu.program_counter = 0x67;
+        cpu.current_instruction = Instruction::LDA;
+        cpu.current_addressing_mode = AddressingMode::Imm;
+
+        cpu.tick_rising(&mut address_bus, &mut rw_line);
+        data_bus.set_combined(0b10010110);
+        cpu.tick_falling(&mut data_bus);
+
+        assert_eq!(cpu.accumulator, 0b10010110);
+        assert_eq!(cpu.get_negative_flag(), true);
+        assert_eq!(cpu.get_zero_flag(), false);
     }
 }
