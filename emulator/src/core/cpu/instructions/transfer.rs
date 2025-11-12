e3b87bbd9c0214aa991bf2034d4886795de00015
GP2015 <adamcunningham2015@hotmail.com>
2025-11-12 12:06:04 +0000

    Added load and store unit tests

--- emulator/src/core/cpu/instructions/transfer.rs
+++ emulator/src/core/cpu/instructions/transfer.rs
@@ -67,4 +67,36 @@ mod tests {
         assert_eq!(cpu.get_negative_flag(), true);
         assert_eq!(cpu.get_zero_flag(), false);
     }
+
+    #[test]
+    fn ldx() {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_test_objects();
+        cpu.program_counter = 0x67;
+        cpu.current_instruction = Instruction::LDX;
+        cpu.current_addressing_mode = AddressingMode::Imm;
+
+        tick_rising_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        data_bus.set_combined(0b10010110);
+        tick_falling_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+
+        assert_eq!(cpu.x_register, 0b10010110);
+        assert_eq!(cpu.get_negative_flag(), true);
+        assert_eq!(cpu.get_zero_flag(), false);
+    }
+
+    #[test]
+    fn ldy() {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_test_objects();
+        cpu.program_counter = 0x67;
+        cpu.current_instruction = Instruction::LDY;
+        cpu.current_addressing_mode = AddressingMode::Imm;
+
+        tick_rising_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        data_bus.set_combined(0b10010110);
+        tick_falling_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+
+        assert_eq!(cpu.y_register, 0b10010110);
+        assert_eq!(cpu.get_negative_flag(), true);
+        assert_eq!(cpu.get_zero_flag(), false);
+    }
 }
