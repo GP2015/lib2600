316c452e4225976fe22268ddafd8b92472f04625
GP2015 <adamcunningham2015@hotmail.com>
2025-11-18 11:25:45 +0000

    Added register increment/decrement unit tests

--- emulator/src/core/cpu/instructions/increment.rs
+++ emulator/src/core/cpu/instructions/increment.rs
@@ -31,3 +31,87 @@ pub fn inc_reg_rise(reg: Register, inc_type: Type, cpu: &mut CPU, lines: &mut CP
 pub fn inc_reg_fall(cpu: &mut CPU) {
     cpu.end_instruction();
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::core::ReadOrWrite;
+    use crate::core::cpu::instructions::increment::Type;
+    use crate::core::cpu::instructions::{Instruction, Register};
+    use crate::core::cpu::test_functions::*;
+
+    #[test]
+    fn inx() {
+        inc_reg_generic(Register::X, Type::Inc, Instruction::INX);
+    }
+
+    #[test]
+    fn iny() {
+        inc_reg_generic(Register::Y, Type::Inc, Instruction::INY);
+    }
+
+    #[test]
+    fn dex() {
+        inc_reg_generic(Register::X, Type::Dec, Instruction::DEX);
+    }
+
+    #[test]
+    fn dey() {
+        inc_reg_generic(Register::Y, Type::Dec, Instruction::DEY);
+    }
+
+    fn inc_reg_generic(reg: Register, inc_type: Type, instruction: Instruction) {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_test_objects();
+        cpu.program_counter = 0x67;
+        cpu.x_register = 0x12;
+        cpu.y_register = 0x34;
+        cpu.current_instruction = instruction;
+
+        tick_rise_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        assert_eq!(address_bus.get_combined(), 0x67);
+        assert_eq!(rw_line, ReadOrWrite::Read);
+
+        tick_fall_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        assert_eq!(cpu.program_counter, 0x67);
+        assert_eq!(cpu.current_instruction, Instruction::Fetch);
+
+        match reg {
+            Register::X => {
+                assert_eq!(cpu.y_register, 0x34);
+                match inc_type {
+                    Type::Inc => assert_eq!(cpu.x_register, 0x13),
+                    Type::Dec => assert_eq!(cpu.x_register, 0x11),
+                }
+            }
+            Register::Y => {
+                assert_eq!(cpu.x_register, 0x12);
+                match inc_type {
+                    Type::Inc => assert_eq!(cpu.y_register, 0x35),
+                    Type::Dec => assert_eq!(cpu.y_register, 0x33),
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn inc_reg_wrapping() {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_test_objects();
+        cpu.x_register = 0xFF;
+        cpu.current_instruction = Instruction::INX;
+
+        tick_rise_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        tick_fall_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        assert_eq!(cpu.x_register, 0x00);
+    }
+
+    #[test]
+    fn dec_reg_wrapping() {
+        let (mut cpu, mut address_bus, mut data_bus, mut rw_line) = create_test_objects();
+        cpu.x_register = 0x00;
+        cpu.current_instruction = Instruction::DEX;
+
+        tick_rise_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        tick_fall_test(&mut cpu, &mut address_bus, &mut data_bus, &mut rw_line);
+        assert_eq!(cpu.x_register, 0xFF);
+    }
+}
