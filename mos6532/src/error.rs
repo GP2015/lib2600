1715f46a384eb2a5bb9e350deceda403761ea264
GP2015 <adamcunningham2015@hotmail.com>
2025-12-28 16:57:22 +0000

    Added RIOTError doc comments

--- mos6532/src/error.rs
+++ mos6532/src/error.rs
@@ -1,16 +1,52 @@
 use thiserror::Error;
 
+/// Indicates some failure within the emulated MOS 6532 RIOT chip.
 #[derive(Error, Debug)]
 pub enum RIOTError {
+    /// Indicates an attempt to access a pin's value before the pin was initialised.
+    ///
+    /// Pins must first be driven with a particular value
+    /// before their value can be accessed (internally or externally).
     #[error("pin is uninitialised")]
     UninitialisedPin,
 
-    #[error("cannot read bit {bit} of {bus_size}-bit bus")]
-    BusBitOutOfRange { bit: usize, bus_size: usize },
+    /// Indicates an attempt to read/drive an invalid pin of a bus.
+    ///
+    /// Each bus has a predefined number of bits/pins,
+    /// so attempting to use a non-existent pin will return this error.
+    #[error("cannot use bit {bit} of {bus_size}-bit bus")]
+    BusBitOutOfRange {
+        /// The bit/pin in question (where the least-significant bit would be 0).
+        bit: usize,
 
+        /// The predefined number of bits in the bus.
+        bus_size: usize,
+    },
+
+    /// Indicates an attempt to drive a value to a bus which cannot fit cleanly without wrapping.
+    ///
+    /// Each bus has a predefined number of bits,
+    /// so if the value being driven requires more bits to represent than the bus has,
+    /// this error will be returned when wrapping is not used.
     #[error("cannot drive value {value} to a {bus_size}-bit bus without wrapping")]
-    BusDriveValueTooLarge { value: usize, bus_size: usize },
+    BusDriveValueTooLarge {
+        /// The value attempting to be driven.
+        value: usize,
 
+        /// The predefined number of bits in the bus.
+        bus_size: usize,
+    },
+
+    /// Indicates an attempt to access a bus pin's value before the pin was initialised.
+    ///
+    /// Bus pins must first be driven with a particular value
+    /// before their value can be accessed (internally or externally).
+    ///
+    /// Only pins being accessed need to be initialised.
+    /// Other pins in the bus can be left uninitialised without this error being returned.
     #[error("bit {bit} of bus is uninitialised")]
-    UninitialisedBusBit { bit: usize },
+    UninitialisedBusBit {
+        /// The bit/pin in question (where the least-significant bit would be 0).
+        bit: usize,
+    },
 }
