63535c21742a679f8d0d4b95f4f1e07c052a3e31
GP2015 <adamcunningham2015@hotmail.com>
2026-01-01 21:46:34 +0000

    Added RIOT RAM unit tests

--- mos6532/src/ram.rs
+++ mos6532/src/ram.rs
@@ -1,12 +1,16 @@
 use crate::RIOTError;
 
+const RAM_SIZE: usize = 128;
+
 pub struct RAM {
-    bytes: [Option<u8>; 128],
+    bytes: [Option<u8>; RAM_SIZE],
 }
 
 impl RAM {
     pub fn new() -> Self {
-        Self { bytes: [None; 128] }
+        Self {
+            bytes: [None; RAM_SIZE],
+        }
     }
 
     pub fn write_byte(&mut self, addr: usize, byte: u8) {
@@ -18,5 +22,26 @@ impl RAM {
             Some(byte) => Ok(byte),
             None => Err(RIOTError::UninitialisedRAMByte { address: addr }),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_write_byte() {
+        let mut ram = RAM::new();
+        ram.write_byte(0, 0x67);
+        ram.write_byte(127, 0x89);
+        assert_eq!(ram.read_byte(0).unwrap(), 0x67);
+        assert_eq!(ram.read_byte(127).unwrap(), 0x89);
+    }
+
+    #[test]
+    fn read_uninitialised_byte() {
+        let mut ram = RAM::new();
+        ram.write_byte(23, 0x67);
+        assert!(ram.read_byte(45).is_err());
     }
 }
