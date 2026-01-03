7d9b0ad02346134a82d7bdbd4f76b2cc83b15ec5
GP2015 <adamcunningham2015@hotmail.com>
2026-01-03 17:31:33 +0000

    Added public RIOT is_driven methods

--- mos6532/src/lib.rs
+++ mos6532/src/lib.rs
@@ -52,13 +52,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusDriveValueTooLarge`]
     /// if `val` cannot fit in the address bus without wrapping.
-    pub fn drv_a(&mut self, val: usize) -> Result<(), RIOTError> {
+    pub fn drive_a(&mut self, val: usize) -> Result<(), RIOTError> {
         self.buf.a.drive(val)
     }
 
     /// Drive the address bus with the value `val`,
     /// wrapping if necessary.
-    pub fn drv_a_wrap(&mut self, val: usize) {
+    pub fn drive_a_wrap(&mut self, val: usize) {
         self.buf.a.drive_wrap(val);
     }
 
@@ -66,8 +66,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusBitOutOfRange`]
     /// if the address bus has no bit `bit`.
-    pub fn drv_a_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
+    pub fn drive_a_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
         self.buf.a.drive_bit(bit, state)
+    }
+
+    /// Returns true if the address bus is being driven with a value.
+    pub fn a_driven(&self) -> bool {
+        self.buf.a.is_driven()
     }
 
     // Data Bus operations
@@ -76,7 +81,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the data bus are still uninitialised.
-    pub fn rd_db(&self) -> Result<usize, RIOTError> {
+    pub fn read_db(&self) -> Result<usize, RIOTError> {
         self.buf.db.read()
     }
 
@@ -87,7 +92,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the data bus are still uninitialised.
-    pub fn rd_db_bit(&self, bit: usize) -> Result<bool, RIOTError> {
+    pub fn read_db_bit(&self, bit: usize) -> Result<bool, RIOTError> {
         self.buf.db.read_bit(bit)
     }
 
@@ -96,13 +101,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusDriveValueTooLarge`]
     /// if `val` cannot fit in the data bus without wrapping.
-    pub fn drv_db(&mut self, val: usize) -> Result<(), RIOTError> {
+    pub fn drive_db(&mut self, val: usize) -> Result<(), RIOTError> {
         self.buf.db.drive(val)
     }
 
     /// Drive the data bus with the value `val`,
     /// wrapping if necessary.
-    pub fn drv_db_wrap(&mut self, val: usize) {
+    pub fn drive_db_wrap(&mut self, val: usize) {
         self.buf.db.drive_wrap(val);
     }
 
@@ -110,8 +115,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusBitOutOfRange`]
     /// if the data bus has no bit `bit`.
-    pub fn drv_db_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
+    pub fn drive_db_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
         self.buf.db.drive_bit(bit, state)
+    }
+
+    /// Returns true if the data bus is being driven with a value.
+    pub fn db_driven(&self) -> bool {
+        self.buf.db.is_driven()
     }
 
     // Peripheral A Data operations
@@ -120,7 +130,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the Peripheral A data bus are still uninitialised.
-    pub fn rd_pa(&self) -> Result<usize, RIOTError> {
+    pub fn read_pa(&self) -> Result<usize, RIOTError> {
         self.buf.pa.read()
     }
 
@@ -131,7 +141,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the Peripheral A data bus are still uninitialised.
-    pub fn rd_pa_bit(&self, bit: usize) -> Result<bool, RIOTError> {
+    pub fn read_pa_bit(&self, bit: usize) -> Result<bool, RIOTError> {
         self.buf.pa.read_bit(bit)
     }
 
@@ -140,13 +150,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusDriveValueTooLarge`]
     /// if `val` cannot fit in the Peripheral A data bus without wrapping.
-    pub fn drv_pa(&mut self, val: usize) -> Result<(), RIOTError> {
+    pub fn drive_pa(&mut self, val: usize) -> Result<(), RIOTError> {
         self.buf.pa.drive(val)
     }
 
     /// Drive the Peripheral A data bus with the value `val`,
     /// wrapping if necessary.
-    pub fn drv_pa_wrap(&mut self, val: usize) {
+    pub fn drive_pa_wrap(&mut self, val: usize) {
         self.buf.pa.drive_wrap(val);
     }
 
@@ -154,8 +164,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusBitOutOfRange`]
     /// if the Peripheral A data bus has no bit `bit`.
-    pub fn drv_pa_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
+    pub fn drive_pa_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
         self.buf.pa.drive_bit(bit, state)
+    }
+
+    /// Returns true if the Peripheral A data bus is being driven with a value.
+    pub fn pa_driven(&self) -> bool {
+        self.buf.pa.is_driven()
     }
 
     // Peripheral B Data operations
@@ -164,7 +179,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the Peripheral B data bus are still uninitialised.
-    pub fn rd_pb(&self) -> Result<usize, RIOTError> {
+    pub fn read_pb(&self) -> Result<usize, RIOTError> {
         self.buf.pb.read()
     }
 
@@ -175,7 +190,7 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::UninitialisedBusBit`]
     /// if any of the bits on the Peripheral B data bus are still uninitialised.
-    pub fn rd_pb_bit(&self, bit: usize) -> Result<bool, RIOTError> {
+    pub fn read_pb_bit(&self, bit: usize) -> Result<bool, RIOTError> {
         self.buf.pb.read_bit(bit)
     }
 
@@ -184,13 +199,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusDriveValueTooLarge`]
     /// if `val` cannot fit in the Peripheral B data bus without wrapping.
-    pub fn drv_pb(&mut self, val: usize) -> Result<(), RIOTError> {
+    pub fn drive_pb(&mut self, val: usize) -> Result<(), RIOTError> {
         self.buf.pb.drive(val)
     }
 
     /// Drive the Peripheral B data bus with the value `val`,
     /// wrapping if necessary.
-    pub fn drv_pb_wrap(&mut self, val: usize) {
+    pub fn drive_pb_wrap(&mut self, val: usize) {
         self.buf.pb.drive_wrap(val);
     }
 
@@ -198,8 +213,13 @@ impl RIOT {
     ///
     /// Returns a [`RIOTError::BusBitOutOfRange`]
     /// if the Peripheral B data bus has no bit `bit`.
-    pub fn drv_pb_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
+    pub fn drive_pb_bit(&mut self, bit: usize, state: bool) -> Result<(), RIOTError> {
         self.buf.pb.drive_bit(bit, state)
+    }
+
+    /// Returns true if the Peripheral B data bus is being driven with a value.
+    pub fn pb_driven(&self) -> bool {
+        self.buf.pb.is_driven()
     }
 
     // Other pin operations
@@ -210,35 +230,65 @@ impl RIOT {
     }
 
     /// Drive the Chip Select 1 pin with state `state`.
-    pub fn drv_cs1(&mut self, state: bool) {
+    pub fn drive_cs1(&mut self, state: bool) {
         self.buf.cs1.drive(state)
     }
 
+    /// Returns true if the Chip Select 1 pin is being driven.
+    pub fn cs1_driven(&self) -> bool {
+        self.buf.cs1.is_driven()
+    }
+
     /// Drive the Chip Select 2 pin with state `state`.
-    pub fn drv_cs2(&mut self, state: bool) {
+    pub fn drive_cs2(&mut self, state: bool) {
         self.buf.cs2.drive(state)
     }
 
+    /// Returns true if the Chip Select 2 pin is being driven.
+    pub fn cs2_driven(&self) -> bool {
+        self.buf.cs2.is_driven()
+    }
+
     /// Drive the Read/Write pin with state `state`.
-    pub fn drv_rw(&mut self, state: bool) {
+    pub fn drive_rw(&mut self, state: bool) {
         self.buf.rw.drive(state)
     }
 
+    /// Returns true if the Read/Write pin is being driven.
+    pub fn rw_driven(&self) -> bool {
+        self.buf.rw.is_driven()
+    }
+
     /// Drive the Reset pin with state `state`.
-    pub fn drv_res(&mut self, state: bool) {
+    pub fn drive_res(&mut self, state: bool) {
         self.buf.res.drive(state)
     }
 
+    /// Returns true if the Reset pin is being driven.
+    pub fn res_driven(&self) -> bool {
+        self.buf.res.is_driven()
+    }
+
     /// Drive the Ram Select pin with state `state`.
-    pub fn drv_rs(&mut self, state: bool) {
+    pub fn drive_rs(&mut self, state: bool) {
         self.buf.rs.drive(state)
+    }
+
+    /// Returns true if the Ram Select pin is being driven.
+    pub fn rs_driven(&self) -> bool {
+        self.buf.rs.is_driven()
     }
 
     /// Read the Interrupt Request pin.
     ///
     /// Returns a [`RIOTError::UninitialisedPin`]
     /// if the pin is still uninitialised.
-    pub fn rd_irq(&self) -> Result<bool, RIOTError> {
+    pub fn read_irq(&self) -> Result<bool, RIOTError> {
         self.buf.irq.read()
+    }
+
+    /// Returns true if the Interrupt Request pin is being driven.
+    pub fn irq_driven(&self) -> bool {
+        self.buf.irq.is_driven()
     }
 }
