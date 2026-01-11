fb7ed94d84d6370e3f4f2497f5e4664a2e57495f
GP2015 <adamcunningham2015@hotmail.com>
2026-01-11 15:25:41 +0000

    Fixed RIOT instruction decode bug

--- mos6532/src/control.rs
+++ mos6532/src/control.rs
@@ -24,7 +24,7 @@ impl Riot {
         Ok(())
     }
 
-    pub fn decode_execute_instruction(&mut self) -> Result<(), RiotError> {
+    fn decode_execute_instruction(&mut self) -> Result<(), RiotError> {
         if self.buf.rs.read()? {
             if self.buf.a.read_bit(2)? {
                 if self.buf.rw.read()? {
@@ -60,7 +60,7 @@ impl Riot {
                 } else {
                     self.write_ddr(AOrB::A)?
                 }
-            } else if self.buf.a.read_bit(0)? {
+            } else if self.buf.a.read_bit(1)? {
                 if self.buf.rw.read()? {
                     self.read_orb()?
                 } else {
@@ -76,72 +76,6 @@ impl Riot {
         } else {
             self.write_ram()?
         }
-
-        // if self.buf.rs.read()? {
-        //     if self.buf.a.read_bit(2)? {
-        //         if self.buf.rw.read()? {
-        //             if self.buf.a.read_bit(0)? {
-        //                 // Read interrupt flags
-        //             } else {
-        //                 // Read timer (uses A3)
-        //             }
-        //         } else {
-        //             if self.buf.a.read_bit(4)? {
-        //                 if self.buf.a.read_bit(1)? {
-        //                     if self.buf.a.read_bit(0)? {
-        //                         // Write timer +1024T (uses A3)
-        //                     } else {
-        //                         // Write timer +64T (uses A3)
-        //                     }
-        //                 } else {
-        //                     if self.buf.a.read_bit(0)? {
-        //                         // Write timer +8T (uses A3)
-        //                     } else {
-        //                         // Write timer +1T (uses A3)
-        //                     }
-        //                 }
-        //             } else {
-        //                 // Write edge detect control (uses A1 and A0)
-        //             }
-        //         }
-        //     } else {
-        //         if self.buf.a.read_bit(0)? {
-        //             if self.buf.a.read_bit(1)? {
-        //                 if self.buf.rw.read()? {
-        //                     self.read_ddr(AOrB::B)?
-        //                 } else {
-        //                     self.write_ddr(AOrB::B)?
-        //                 }
-        //             } else {
-        //                 if self.buf.rw.read()? {
-        //                     self.read_ddr(AOrB::A)?
-        //                 } else {
-        //                     self.write_ddr(AOrB::A)?
-        //                 }
-        //             }
-        //         } else {
-        //             if self.buf.a.read_bit(0)? {
-        //                 if self.buf.rw.read()? {
-        //                     self.read_orb()?
-        //                 } else {
-        //                     self.write_or(AOrB::B)?
-        //                 }
-        //             } else {
-        //                 if self.buf.rw.read()? {
-        //                     self.read_ora()?
-        //                 } else {
-        //                     self.write_or(AOrB::A)?
-        //                 }
-        //             }
-        //         }
-        //     }
-        // } else {
-        //     if self.buf.rw.read()? {
-        //         self.read_ram()?
-        //     } else {
-        //         self.write_ram()?
-        //     }
-        // }
 
         Ok(())
     }
