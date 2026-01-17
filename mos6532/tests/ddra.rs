703c7ffd11922e11bb64adcbc628a308d9ec2802
GP2015 <adamcunningham2015@hotmail.com>
2026-01-17 21:45:52 +0000

    Started RIOT DDR tests

---
+++ mos6532/tests/ddra.rs
@@ -0,0 +1,45 @@
+mod common;
+
+#[test]
+fn read_write_ddra_success() {
+    let mut riot = common::riot_post_reset();
+    riot.write_ddra_pulse(0x67).unwrap();
+    assert_eq!(riot.read_ddra_pulse().unwrap(), 0x67);
+}
+
+#[test]
+fn read_uninitialised_ddra() {
+    let mut riot = common::riot_post_reset();
+    assert_eq!(riot.read_ddra_pulse().unwrap(), 0);
+}
+
+#[test]
+fn read_ddra_no_rs() {
+    let mut riot = common::riot_post_reset_select();
+    riot.write_a_bit(2, false).unwrap();
+    riot.write_a_bit(0, true).unwrap();
+    riot.write_a_bit(1, false).unwrap();
+    riot.write_a_bit(, false).unwrap();
+    assert!(riot.pulse_phi2().is_err());
+}
+
+// #[test]
+// fn read_ram_no_rs() {
+//     let mut riot = common::riot_post_reset_select();
+//     riot.write_rw(false);
+//     assert!(riot.pulse_phi2().is_err());
+// }
+
+// #[test]
+// fn write_ram_no_rs() {
+//     let mut riot = common::riot_post_reset_select();
+//     riot.write_rw(true);
+//     assert!(riot.pulse_phi2().is_err());
+// }
+
+// #[test]
+// fn use_ram_no_rw() {
+//     let mut riot = common::riot_post_reset_select();
+//     riot.write_rs(true);
+//     assert!(riot.pulse_phi2().is_err());
+// }
