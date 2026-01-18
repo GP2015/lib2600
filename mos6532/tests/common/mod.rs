fe6ebe2879ee10d7fae564d845f94d6d8ca315ca
GP2015 <adamcunningham2015@hotmail.com>
2026-01-18 23:38:52 +0000

    Removed unused RIOT integration test helpers

--- mos6532/tests/common/mod.rs
+++ mos6532/tests/common/mod.rs
@@ -20,9 +20,3 @@ pub fn riot_post_select() -> Riot {
     riot.select();
     riot
 }
-
-pub fn write_a2_to_a0(riot: &mut Riot, a2: bool, a1: bool, a0: bool) {
-    riot.write_a_bit(2, a2).unwrap();
-    riot.write_a_bit(1, a1).unwrap();
-    riot.write_a_bit(0, a0).unwrap();
-}
