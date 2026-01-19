4e0eb336121d6f171cdd637a4ed26150a20e018c
GP2015 <adamcunningham2015@hotmail.com>
2026-01-19 15:57:34 +0000

    Decoupled RIOT instruction decoding and executing

--- mos6532/src/control.rs
+++ mos6532/src/control.rs
@@ -6,6 +6,8 @@ mod timer;
 
 use crate::{Riot, RiotError};
 
+type InstructionPtr = fn(&mut Riot) -> Result<(), RiotError>;
+
 impl Riot {
     pub(super) fn tick(&mut self) -> Result<(), RiotError> {
         if !self.buf.res.read()? {
@@ -19,66 +21,67 @@ impl Riot {
 
         self.update_edc()?;
 
-        self.decode_execute_instruction()?;
-
-        Ok(())
+        let instruction = self.decode_instruction()?;
+        self.execute_instruction(instruction)
     }
 
-    fn decode_execute_instruction(&mut self) -> Result<(), RiotError> {
-        if self.buf.rs.read()? {
+    fn decode_instruction(&mut self) -> Result<InstructionPtr, RiotError> {
+        let instruction = if self.buf.rs.read()? {
             if self.buf.a.read_bit(2)? {
                 if self.buf.rw.read()? {
                     if self.buf.a.read_bit(0)? {
-                        self.read_interrupt_flag()?;
+                        Self::read_interrupt_flag
                     } else {
-                        // Read timer (uses A3)
+                        Self::read_timer
                     }
                 } else if self.buf.a.read_bit(4)? {
                     if self.buf.a.read_bit(1)? {
                         if self.buf.a.read_bit(0)? {
-                            // Write timer +1024T (uses A3)
+                            Self::write_timer_1024t
                         } else {
-                            // Write timer +64T (uses A3)
+                            Self::write_timer_64t
                         }
                     } else if self.buf.a.read_bit(0)? {
-                        // Write timer +8T (uses A3)
+                        Self::write_timer_8t
                     } else {
-                        // Write timer +1T (uses A3)
+                        Self::write_timer_1t
                     }
                 } else {
-                    let enable_irq = self.buf.a.read_bit(1)?;
-                    let use_pos_edge = self.buf.a.read_bit(0)?;
-                    self.write_edc(enable_irq, use_pos_edge);
+                    Self::write_edc
                 }
             } else if self.buf.a.read_bit(0)? {
                 if self.buf.a.read_bit(1)? {
                     if self.buf.rw.read()? {
-                        self.read_ddrb()?
+                        Self::read_ddrb
                     } else {
-                        self.write_ddrb()?
+                        Self::write_ddrb
                     }
                 } else if self.buf.rw.read()? {
-                    self.read_ddra()?
+                    Self::read_ddra
                 } else {
-                    self.write_ddra()?
+                    Self::write_ddra
                 }
             } else if self.buf.a.read_bit(1)? {
                 if self.buf.rw.read()? {
-                    self.read_orb()?
+                    Self::read_orb
                 } else {
-                    self.write_orb()?
+                    Self::write_orb
                 }
             } else if self.buf.rw.read()? {
-                self.read_ora()?
+                Self::read_ora
             } else {
-                self.write_ora()?
+                Self::write_ora
             }
         } else if self.buf.rw.read()? {
-            self.read_ram()?
+            Self::read_ram
         } else {
-            self.write_ram()?
-        }
+            Self::write_ram
+        };
 
-        Ok(())
+        Ok(instruction)
+    }
+
+    fn execute_instruction(&mut self, instruction: InstructionPtr) -> Result<(), RiotError> {
+        instruction(self)
     }
 }
