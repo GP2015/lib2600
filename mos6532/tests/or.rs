010d4f9dfa28cf9e623c9a5db57687b07c57fda5
GP2015 <adamcunningham2015@hotmail.com>
2026-01-21 15:41:29 +0000

    Finished RIOT OR integration tests

--- mos6532/tests/or.rs
+++ mos6532/tests/or.rs
@@ -113,12 +113,77 @@ fn read_mixed_p(#[case] reg: bool, #[case] ddr: u8, #[case] out: u8) {
 #[rstest]
 #[case(ATYPE)]
 #[case(BTYPE)]
-fn output_p_update_deselected(#[case] reg: bool) {
+fn output_p_update_on_deselected_pulse(#[case] reg: bool) {
     let mut riot = common::riot_post_reset();
     write_ddr_pulse(&mut riot, reg, 0xFF).unwrap();
     write_or_pulse(&mut riot, reg, 0x67).unwrap();
     write_p(&mut riot, reg, 0x89);
     assert_eq!(read_p(&mut riot, reg).unwrap(), 0x89);
-    riot.deselected_pulse().unwrap();
+    riot.write_cs1(false);
+    riot.pulse_phi2().unwrap();
     assert_eq!(read_p(&mut riot, reg).unwrap(), 0x67);
+}
+
+#[rstest]
+#[case(ATYPE, false)]
+#[case(BTYPE, true)]
+fn write_output_p_manual(#[case] reg: bool, #[case] a1: bool) {
+    let mut riot = common::riot_post_reset_select();
+    write_ddr_pulse(&mut riot, reg, 0xFF).unwrap();
+    riot.write_rs(true);
+    riot.write_rw(false);
+    riot.write_a_bit(2, false).unwrap();
+    riot.write_a_bit(1, a1).unwrap();
+    riot.write_a_bit(0, false).unwrap();
+    riot.write_db(0x67);
+    riot.pulse_phi2().unwrap();
+    assert_eq!(read_p(&mut riot, reg).unwrap(), 0x67);
+}
+
+#[rstest]
+#[case(ATYPE, false)]
+#[case(BTYPE, true)]
+fn read_input_p_manual(#[case] reg: bool, #[case] a1: bool) {
+    let mut riot = common::riot_post_reset_select();
+    write_p(&mut riot, reg, 0x67);
+    riot.write_rs(true);
+    riot.write_rw(true);
+    riot.write_a_bit(2, false).unwrap();
+    riot.write_a_bit(1, a1).unwrap();
+    riot.write_a_bit(0, false).unwrap();
+    riot.pulse_phi2().unwrap();
+    assert_eq!(riot.read_db().unwrap(), 0x67);
+}
+
+#[rstest]
+#[case(ATYPE, false)]
+#[case(BTYPE, true)]
+fn write_output_p_deselected(#[case] reg: bool, #[case] a1: bool) {
+    let mut riot = common::riot_post_reset_select();
+    write_ddr_pulse(&mut riot, reg, 0xFF).unwrap();
+    riot.write_cs1(false);
+    riot.write_rs(true);
+    riot.write_rw(false);
+    riot.write_a_bit(2, false).unwrap();
+    riot.write_a_bit(1, a1).unwrap();
+    riot.write_a_bit(0, false).unwrap();
+    riot.write_db(0x67);
+    riot.pulse_phi2().unwrap();
+    assert_eq!(read_p(&mut riot, reg).unwrap(), 0);
+}
+
+#[rstest]
+#[case(ATYPE, false)]
+#[case(BTYPE, true)]
+fn read_input_p_deselected(#[case] reg: bool, #[case] a1: bool) {
+    let mut riot = common::riot_post_reset_select();
+    write_p(&mut riot, reg, 0x67);
+    riot.write_cs1(false);
+    riot.write_rs(true);
+    riot.write_rw(true);
+    riot.write_a_bit(2, false).unwrap();
+    riot.write_a_bit(1, a1).unwrap();
+    riot.write_a_bit(0, false).unwrap();
+    riot.pulse_phi2().unwrap();
+    assert!(riot.read_db().is_err());
 }
