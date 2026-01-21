b9fc9ecaa17b5c7b4774c92e0e23346e4c8fd99e
GP2015 <adamcunningham2015@hotmail.com>
2026-01-21 18:12:25 +0000

    Added RIOT RAM unit tests

--- mos6532/src/data/ram.rs
+++ mos6532/src/data/ram.rs
@@ -32,20 +32,38 @@ impl Ram {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::{fixture, rstest};
 
-    #[test]
-    fn read_write_byte() {
-        let mut ram = Ram::new();
+    #[fixture]
+    fn ram() -> Ram {
+        Ram::new()
+    }
+
+    #[rstest]
+    fn read_write_byte(mut ram: Ram) {
         ram.write_byte(0, 0x67);
         ram.write_byte(127, 0x89);
         assert_eq!(ram.read_byte(0).unwrap(), 0x67);
         assert_eq!(ram.read_byte(127).unwrap(), 0x89);
     }
 
-    #[test]
-    fn read_uninitialised_byte() {
-        let mut ram = Ram::new();
+    #[rstest]
+    fn overwrite_byte(mut ram: Ram) {
+        ram.write_byte(23, 0x67);
+        ram.write_byte(23, 0x89);
+        assert_eq!(ram.read_byte(23).unwrap(), 0x89);
+    }
+
+    #[rstest]
+    fn read_uninitialised_byte(mut ram: Ram) {
         ram.write_byte(23, 0x67);
         assert!(ram.read_byte(45).is_err());
+    }
+
+    #[rstest]
+    fn reset_byte(mut ram: Ram) {
+        ram.write_byte(23, 0x67);
+        ram.reset();
+        assert!(ram.read_byte(23).is_err());
     }
 }
