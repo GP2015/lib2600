b8ee348fcae7c1c2aac2c67ed0ac861616dba242
GP2015 <adamcunningham2015@hotmail.com>
2026-01-21 00:59:10 +0000

    Added more RIOT OR integration tests

--- mos6532/tests/or.rs
+++ mos6532/tests/or.rs
@@ -1,8 +1,7 @@
 mod common;
-// use rstest::rstest;
 
 #[test]
-fn output_to_p() {
+fn write_output_p() {
     let mut riot = common::riot_post_reset();
     riot.write_ddra_pulse(0xFF).unwrap();
     riot.write_ddrb_pulse(0xFF).unwrap();
@@ -13,7 +12,7 @@ fn output_to_p() {
 }
 
 #[test]
-fn output_to_p_input_ddr() {
+fn write_input_p() {
     let mut riot = common::riot_post_reset();
     riot.write_pa(0x23);
     riot.write_pb(0x45);
@@ -28,98 +27,65 @@ fn output_to_p_input_ddr() {
 }
 
 #[test]
-fn input_to_p() {
+fn write_mixed_p() {
     let mut riot = common::riot_post_reset();
     riot.write_pa(0x23);
-    riot.write_pb(0x67);
-    riot.write_ora_pulse(0x45).unwrap();
+    riot.write_pb(0x45);
+    riot.write_ddra_pulse(0x0F).unwrap();
+    riot.write_ddrb_pulse(0xF0).unwrap();
+    riot.write_ora_pulse(0x67).unwrap();
+    riot.write_orb_pulse(0x89).unwrap();
+    assert_eq!(riot.read_pa().unwrap(), 0x27);
+    assert_eq!(riot.read_pb().unwrap(), 0x85);
+}
+
+#[test]
+fn read_input_p() {
+    let mut riot = common::riot_post_reset();
+    riot.write_pa(0x23);
+    riot.write_pb(0x45);
+    riot.write_ora_pulse(0x67).unwrap();
     riot.write_orb_pulse(0x89).unwrap();
     assert_eq!(riot.read_ora_pulse().unwrap(), 0x23);
-    assert_eq!(riot.read_orb_pulse().unwrap(), 0x67);
+    assert_eq!(riot.read_orb_pulse().unwrap(), 0x45);
 }
 
 #[test]
-fn input_to_pa_output_ddra() {
+fn read_output_p() {
     let mut riot = common::riot_post_reset();
     riot.write_ddra_pulse(0xFF).unwrap();
-    riot.write_orb_pulse(0x67).unwrap();
-    riot.write_pa(0x89);
-    assert_eq!(riot.read_ora_pulse().unwrap(), 0x89);
-}
-
-#[test]
-fn input_to_pb_output_ddrb() {
-    let mut riot = common::riot_post_reset();
     riot.write_ddrb_pulse(0xFF).unwrap();
-    riot.write_orb_pulse(0x67).unwrap();
-    riot.write_pb(0x89);
-    assert_eq!(riot.read_orb_pulse().unwrap(), 0x67);
-    riot.write_ddrb_pulse(0).unwrap();
+    riot.write_ora_pulse(0x67).unwrap();
+    riot.write_orb_pulse(0x89).unwrap();
+    assert_eq!(riot.read_ora_pulse().unwrap(), 0x67);
     assert_eq!(riot.read_orb_pulse().unwrap(), 0x89);
 }
 
-// #[rstest]
-// #[case(false)]
-// #[case(true)]
-// fn read_write_or_success_manual(#[case] a1: bool) {
-//     let mut riot = common::riot_post_reset_select();
-//     riot.write_rs(true);
-//     riot.write_rw(false);
-//     riot.write_a_bit(2, false).unwrap();
-//     riot.write_a_bit(1, a1).unwrap();
-//     riot.write_a_bit(0, false).unwrap();
-//     riot.write_db(0x67);
-//     riot.pulse_phi2().unwrap();
+#[test]
+fn read_mixed_p() {
+    let mut riot = common::riot_post_reset();
+    riot.write_pa(0x23);
+    riot.write_pb(0x45);
+    riot.write_ddra_pulse(0x0F).unwrap();
+    riot.write_ddrb_pulse(0xF0).unwrap();
+    riot.write_ora_pulse(0x67).unwrap();
+    riot.write_orb_pulse(0x89).unwrap();
+    assert_eq!(riot.read_ora_pulse().unwrap(), 0x27);
+    assert_eq!(riot.read_orb_pulse().unwrap(), 0x85);
+}
 
-//     riot.write_rw(true);
-//     riot.write_db(0x89);
-//     riot.pulse_phi2().unwrap();
-//     assert_eq!(riot.read_db().unwrap(), 0x67);
-// }
-
-// #[rstest]
-// #[case(false)]
-// #[case(true)]
-// fn write_or_deselected(#[case] a1: bool) {
-//     let mut riot = common::riot_post_reset_select();
-//     riot.write_cs1(false);
-//     riot.write_rs(true);
-//     riot.write_rw(false);
-//     riot.write_a_bit(2, false).unwrap();
-//     riot.write_a_bit(1, a1).unwrap();
-//     riot.write_a_bit(0, false).unwrap();
-//     riot.write_db(0x67);
-//     riot.pulse_phi2().unwrap();
-
-//     riot.write_cs1(true);
-//     riot.write_rw(true);
-//     riot.pulse_phi2().unwrap();
-//     assert_eq!(riot.read_db().unwrap(), 0);
-// }
-
-// #[rstest]
-// #[case(false)]
-// #[case(true)]
-// fn read_or_deselected(#[case] a1: bool) {
-//     let mut riot = common::riot_post_reset_select();
-//     riot.write_rs(true);
-//     riot.write_rw(false);
-//     riot.write_a_bit(2, false).unwrap();
-//     riot.write_a_bit(1, a1).unwrap();
-//     riot.write_a_bit(0, false).unwrap();
-//     riot.write_db(0x67);
-//     riot.pulse_phi2().unwrap();
-
-//     riot.write_db(0x89);
-//     riot.write_cs1(false);
-//     riot.write_rw(true);
-//     riot.pulse_phi2().unwrap();
-//     assert_eq!(riot.read_db().unwrap(), 0x89);
-// }
-
-// #[test]
-// fn read_reset_or() {
-//     let mut riot = common::riot_post_reset();
-//     assert_eq!(riot.read_ora_pulse().unwrap(), 0);
-//     assert_eq!(riot.read_orb_pulse().unwrap(), 0);
-// }
+#[test]
+fn output_p_update_deselected() {
+    let mut riot = common::riot_post_reset();
+    riot.write_ddra_pulse(0xFF).unwrap();
+    riot.write_ddrb_pulse(0xFF).unwrap();
+    riot.write_ora_pulse(0x23).unwrap();
+    riot.write_orb_pulse(0x45).unwrap();
+    riot.write_pa(0x67);
+    riot.write_pb(0x89);
+    assert_eq!(riot.read_pa().unwrap(), 0x67);
+    assert_eq!(riot.read_pb().unwrap(), 0x89);
+    riot.deselected_pulse().unwrap();
+    assert_eq!(riot.read_pa().unwrap(), 0x23);
+    assert_eq!(riot.read_pb().unwrap(), 0x45);
+}
