6298983112a18c41842ed55a5c0c81e68b163fea
GP2015 <adamcunningham2015@hotmail.com>
2026-01-24 14:04:45 +0000

    Reset RIOT

--- mos6532/src/data.rs
+++
@@ -1,8 +0,0 @@
-mod buffers;
-mod ram;
-mod registers;
-mod regtype;
-
-pub use buffers::Buffers;
-pub use ram::Ram;
-pub use registers::Registers;
--- mos6532/src/lib.rs
+++ mos6532/src/lib.rs
@@ -1,8 +1,1 @@
-mod control;
-mod data;
-mod error;
-mod helpers;
-mod riot;
 
-pub use crate::error::RiotError;
-pub use crate::riot::Riot;
