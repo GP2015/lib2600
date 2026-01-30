c1d69063e64a3e351fa56e037964944825b6668b
GP2015 <adamcunningham2015@hotmail.com>
2026-01-30 17:11:25 +0000

    Added user-facing RIOT pin getters

--- mos6532/src/data/pins.rs
+++ mos6532/src/data/pins.rs
@@ -3,8 +3,8 @@ pub mod single;
 pub mod state;
 
 use crate::data::pins::{
-    bus::{address::AddressBus, data::ContentionByteBus},
-    single::{SinglePinNew, contention::ContentionPin, input::InputPin},
+    bus::{Bus, address::AddressBus, data::ContentionByteBus},
+    single::{SinglePin, SinglePinNew, contention::ContentionPin, input::InputPin},
 };
 
 type InputPinType = InputPin;
@@ -12,17 +12,25 @@ type OutputPinType = ContentionPin;
 type AddressBusType = AddressBus<InputPin>;
 type TwoWayBusType = ContentionByteBus<ContentionPin>;
 
+macro_rules! pin_getter {
+    ($name:ident, $obj:ident) => {
+        pub fn $name(&mut self) -> &mut impl $obj {
+            &mut self.$name
+        }
+    };
+}
+
 pub struct Pins {
-    pub a: AddressBusType,
-    pub pa: TwoWayBusType,
-    pub pb: TwoWayBusType,
-    pub irq: OutputPinType,
-    pub db: TwoWayBusType,
-    pub res: InputPinType,
-    pub rw: InputPinType,
-    pub rs: InputPinType,
-    pub cs2: InputPinType,
-    pub cs1: InputPinType,
+    pub(crate) a: AddressBusType,
+    pub(crate) db: TwoWayBusType,
+    pub(crate) pa: TwoWayBusType,
+    pub(crate) pb: TwoWayBusType,
+    pub(crate) res: InputPinType,
+    pub(crate) cs1: InputPinType,
+    pub(crate) cs2: InputPinType,
+    pub(crate) rs: InputPinType,
+    pub(crate) rw: InputPinType,
+    pub(crate) irq: OutputPinType,
 }
 
 impl Pins {
@@ -32,12 +40,23 @@ impl Pins {
             db: TwoWayBusType::new(String::from("DB")),
             pa: TwoWayBusType::new(String::from("PA")),
             pb: TwoWayBusType::new(String::from("PB")),
+            res: InputPinType::new(String::from("/RES")),
             cs1: InputPinType::new(String::from("CS1")),
             cs2: InputPinType::new(String::from("/CS2")),
             rw: InputPinType::new(String::from("R/W")),
-            res: InputPinType::new(String::from("/RES")),
             rs: InputPinType::new(String::from("/RS")),
             irq: OutputPinType::new(String::from("/IRQ")),
         }
     }
+
+    pin_getter!(a, Bus);
+    pin_getter!(db, Bus);
+    pin_getter!(pa, Bus);
+    pin_getter!(pb, Bus);
+    pin_getter!(res, SinglePin);
+    pin_getter!(cs1, SinglePin);
+    pin_getter!(cs2, SinglePin);
+    pin_getter!(rw, SinglePin);
+    pin_getter!(rs, SinglePin);
+    pin_getter!(irq, SinglePin);
 }
