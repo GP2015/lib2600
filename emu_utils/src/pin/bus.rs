15a400bb64f237373e54fdf1be4273f498da9241
GP2015 <adamcunningham2015@hotmail.com>
2026-02-02 22:05:31 +0000

    Began implementing previous states for pins in Utils

--- emu_utils/src/pin/bus.rs
+++ emu_utils/src/pin/bus.rs
@@ -1,23 +1,21 @@
 pub mod standard;
 
-use crate::pin::PinState;
+use crate::pin::{PinState, SinglePin};
 
 pub trait Bus {
     type Error;
+    fn pin(&self, bit: usize) -> &impl SinglePin;
+    fn pin_mut(&self, bit: usize) -> &mut impl SinglePin;
     fn state(&self) -> Vec<PinState>;
+    fn prev_state(&self) -> Vec<PinState>;
     fn state_as_bool(&self) -> Vec<Option<bool>>;
+    fn prev_state_as_bool(&self) -> Vec<Option<bool>>;
     fn read(&self) -> Result<usize, Self::Error>;
-    fn bit_state(&self, bit: usize) -> Result<PinState, Self::Error>;
-    fn bit_state_as_bool(&self, bit: usize) -> Result<Option<bool>, Self::Error>;
-    fn read_bit(&self, bit: usize) -> Result<bool, Self::Error>;
+    fn prev(&self) -> Option<usize>;
     fn drive_in(&mut self, val: usize) -> Result<(), Self::Error>;
     fn wrapping_drive_in(&mut self, val: usize) -> Result<(), Self::Error>;
     fn tri_state_in(&mut self);
     fn undefine_in(&mut self) -> Result<(), Self::Error>;
-    fn signal_in_bit(&mut self, bit: usize, state: PinState) -> Result<(), Self::Error>;
-    fn drive_in_bit(&mut self, bit: usize, state: bool) -> Result<(), Self::Error>;
-    fn tri_state_in_bit(&mut self, bit: usize) -> Result<(), Self::Error>;
-    fn undefine_in_bit(&mut self, bit: usize) -> Result<(), Self::Error>;
 }
 
 pub trait BusOutput {
@@ -25,8 +23,4 @@ pub trait BusOutput {
     fn drive_out(&mut self, val: usize) -> Result<(), Self::Error>;
     fn tri_state_out(&mut self);
     fn undefine_out(&mut self) -> Result<(), Self::Error>;
-    fn signal_out_bit(&mut self, bit: usize, state: PinState) -> Result<(), Self::Error>;
-    fn drive_out_bit(&mut self, bit: usize, state: bool) -> Result<(), Self::Error>;
-    fn tri_state_out_bit(&mut self, bit: usize) -> Result<(), Self::Error>;
-    fn undefine_out_bit(&mut self, bit: usize) -> Result<(), Self::Error>;
 }
--- emu_utils/src/pin/single.rs
+++ emu_utils/src/pin/single.rs
@@ -10,8 +10,11 @@ use crate::pin::{PinError, PinState};
 pub trait SinglePin {
     type Error: From<PinError>;
     fn state(&self) -> PinState;
+    fn prev_state(&self) -> PinState;
     fn state_as_bool(&self) -> Option<bool>;
+    fn prev_state_as_bool(&self) -> Option<bool>;
     fn read(&self) -> Result<bool, Self::Error>;
+    fn read_prev(&self) -> Result<bool, Self::Error>;
     fn signal_in(&mut self, state: PinState) -> Result<(), Self::Error>;
     fn drive_in(&mut self, state: bool) -> Result<(), Self::Error>;
     fn tri_state_in(&mut self);
