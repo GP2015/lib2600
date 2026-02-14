08f5c47954f3b2238df55df9e920ddecb4899469
GP2015 <adamcunningham2015@hotmail.com>
2026-02-14 18:12:43 +0000

    Moved Utils RegisterError into separate file

--- emu_utils/src/register.rs
+++ emu_utils/src/register.rs
@@ -1,29 +1,8 @@
 mod bit;
+mod error;
 mod mbit;
 mod val;
 
-pub use crate::register::{bit::BitRegister, mbit::MBitRegister, val::ValueRegister};
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum RegisterError {
-    #[error("cannot access {name} register as it is uninitialised")]
-    ReadUndefined { name: String },
-
-    #[error("cannot access bit {bit} of {name} register as it is uninitialised")]
-    ReadUndefinedBit { name: String, bit: usize },
-
-    #[error("register {name} does not have a bit {bit}")]
-    BitOutOfRange {
-        name: String,
-        bit: usize,
-        size: usize,
-    },
-
-    #[error("cannot write value {value} to {name} register without wrapping")]
-    WriteValueTooLarge {
-        name: String,
-        value: usize,
-        size: usize,
-    },
-}
+pub use crate::register::{
+    bit::BitRegister, error::RegisterError, mbit::MBitRegister, val::ValueRegister,
+};
