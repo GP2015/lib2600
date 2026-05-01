0560d7cfe4a67d61355843865274fe104b7c679c
GP2015 <adamcunningham2015@hotmail.com>
2026-05-01 12:47:00 +0100

    Began adding previous state tracking to RIOT

--- mos6532/src/riot/mod.rs
+++ mos6532/src/riot/mod.rs
@@ -2,22 +2,35 @@ mod instructions;
 mod ram;
 
 use crate::{
-    ClkCycle, RiotError, RiotLineRefs,
+    RiotError, RiotLineRefs,
     riot::{instructions::PossibleInstructions, ram::Ram},
 };
 use emutils::{
-    line::{BusConnection, LineConnection},
+    line::{BusConnection, BusState, LineConnection, LineState},
     reg::{BitRegister, MBitRegister},
 };
 
 #[allow(dead_code)]
 pub struct Riot {
-    clk_cycle: ClkCycle,
     pub(crate) ram: Ram,
+
     pub(crate) db_con: BusConnection,
     pub(crate) pa_con: BusConnection,
     pub(crate) pb_con: BusConnection,
     pub(crate) irq_con: LineConnection,
+
+    a_prev: BusState,
+    db_prev: BusState,
+    pa_prev: BusState,
+    pb_prev: BusState,
+    phi2_prev: LineState,
+    res_prev: LineState,
+    cs1_prev: LineState,
+    cs2_prev: LineState,
+    rs_prev: LineState,
+    rw_prev: LineState,
+    irq_prev: LineState,
+
     pub(crate) ddra: MBitRegister,
     pub(crate) ddrb: MBitRegister,
     pub(crate) ora: MBitRegister,
@@ -37,12 +50,25 @@ impl Riot {
         irq_con: LineConnection,
     ) -> Self {
         Self {
-            clk_cycle: ClkCycle::default(),
             ram: Ram::new(),
+
             db_con,
             pa_con,
             pb_con,
             irq_con,
+
+            a_prev: lines.a.state(),
+            db_prev: lines.db.state(),
+            pa_prev: lines.pa.state(),
+            pb_prev: lines.pb.state(),
+            phi2_prev: lines.phi2.state(),
+            res_prev: lines.res.state(),
+            cs1_prev: lines.cs1.state(),
+            cs2_prev: lines.cs2.state(),
+            rs_prev: lines.rs.state(),
+            rw_prev: lines.rw.state(),
+            irq_prev: lines.irq.state(),
+
             ddra: MBitRegister::new("DDRA", 8),
             ddrb: MBitRegister::new("DDRB", 8),
             ora: MBitRegister::new("ORA", 8),
@@ -54,28 +80,42 @@ impl Riot {
         }
     }
 
-    #[must_use]
-    pub fn clk_cycle(&self) -> ClkCycle {
-        self.clk_cycle
-    }
-
     pub fn tick(&mut self, lines: &mut RiotLineRefs) -> Result<(), RiotError> {
         lines.check_bus_sizes()?;
 
-        self.clk_cycle = match self.clk_cycle {
-            ClkCycle::ClkLow => {
-                let instructions = PossibleInstructions::from(lines);
-                self.execute_possible_instructions(lines, &instructions)?;
-                ClkCycle::ClkHigh
-            }
-            ClkCycle::ClkHigh => {
-                lines
-                    .db
-                    .iter_mut(&self.db_con)
-                    .for_each(|(line, con)| line.add_high_z(con, true));
-                ClkCycle::ClkLow
-            }
-        };
+        let phi2_prev_low = self.phi2_prev.could_read_low();
+        let phi2_prev_high = self.phi2_prev.could_read_high();
+        let phi2_low = lines.phi2.could_read_low();
+        let phi2_high = lines.phi2.could_read_high();
+
+        let phi2_keep_low = phi2_prev_low && phi2_low;
+        let phi2_keep_high = phi2_prev_high && phi2_high;
+        let phi2_rising_edge = phi2_prev_low && phi2_high;
+        let phi2_falling_edge = phi2_prev_high && phi2_low;
+
+        let only_possible = [
+            phi2_keep_low,
+            phi2_keep_high,
+            phi2_rising_edge,
+            phi2_falling_edge,
+        ]
+        .into_iter()
+        .filter(|&b| b)
+        .count()
+            == 1;
+
+        if phi2_rising_edge {
+            let instructions = PossibleInstructions::from(lines);
+            self.execute_possible_instructions(
+                lines,
+                &instructions,
+                only_possible & instructions.only_possible(),
+            )?;
+        }
+
+        if phi2_falling_edge {
+            lines.db.add_high_z(&self.db_con, only_possible);
+        }
 
         Ok(())
     }
@@ -84,9 +124,8 @@ impl Riot {
         &mut self,
         lines: &mut RiotLineRefs,
         instructions: &PossibleInstructions,
+        only_possible: bool,
     ) -> Result<(), RiotError> {
-        let only_possible = instructions.only_possible();
-
         if instructions.reset {
             self.handle_reset(lines, only_possible);
         }
