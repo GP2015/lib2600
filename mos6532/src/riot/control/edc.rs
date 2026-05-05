b2d799a4df63c71ddf1f41118e47bc3ba6a953ec
GP2015 <adamcunningham2015@hotmail.com>
2026-05-05 11:59:24 +0200

    Added RIOT EDC handling

--- mos6532/src/riot/control/edc.rs
+++ mos6532/src/riot/control/edc.rs
@@ -1,33 +1,44 @@
-use crate::{
-    Riot,
-    riot::{lines::RiotOutputLines, states::RiotLineStates},
-};
-use emutils::line::LineError;
+use crate::{Riot, riot::states::RiotLineStates};
 
 impl Riot {
-    pub(crate) fn write_edc(
-        &mut self,
-        lines: &mut RiotOutputLines,
-        states: &RiotLineStates,
-        only_instruction: bool,
-    ) -> Result<(), LineError> {
-        todo!()
+    pub(crate) const fn update_edc(&mut self, states: &RiotLineStates) {
+        let new_pa7_state = states.pa.line_state::<7>();
+
+        let (old_pa7_low, old_pa7_high) = self.old_pa7_state.could_read_low_high();
+        let (new_pa7_low, new_pa7_high) = new_pa7_state.could_read_low_high();
+        let (edc_type_low, edc_type_high) = self.edc_edge_type.low_high_possible();
+
+        if edc_type_high && old_pa7_low && new_pa7_high {
+            let only_transition = !edc_type_low && !old_pa7_high && !new_pa7_low;
+            self.edc_ir_flag.add(true, only_transition);
+        }
+
+        if edc_type_low && old_pa7_high && new_pa7_low {
+            let only_transition = !edc_type_high && !old_pa7_low && !new_pa7_high;
+            self.edc_ir_flag.add(true, only_transition);
+        }
+
+        self.old_pa7_state = new_pa7_state;
     }
 
-    // pub(super) fn write_edc(&mut self, enable_irq: bool, use_pos_edge: bool) {
-    //     self.edc_enable_irq.write(enable_irq);
-    //     self.edc_use_pos_edge.write(use_pos_edge);
-    // }
+    pub(crate) const fn write_edc(&mut self, states: &RiotLineStates, only_instruction: bool) {
+        let a0 = states.a.line_state::<0>();
+        let a1 = states.a.line_state::<1>();
 
-    // pub(super) fn update_edc(&mut self) -> Result<(), LineError> {
-    //     let new_pa7 = self.pa().bit_state(7)?;
+        if a1.could_read_low() {
+            self.edc_enables_irq.add(false, only_instruction);
+        }
 
-    //     if new_pa7 != self.old_pa7 && new_pa7 == self.edc_use_pos_edge.read()? {
-    //         self.edc_interrupt_flag.write(true);
-    //     }
+        if a1.could_read_high() {
+            self.edc_enables_irq.add(true, only_instruction);
+        }
 
-    //     self.old_pa7.write(new_pa7);
+        if a0.could_read_low() {
+            self.edc_edge_type.add(false, only_instruction);
+        }
 
-    //     Ok(())
-    // }
+        if a0.could_read_high() {
+            self.edc_edge_type.add(true, only_instruction);
+        }
+    }
 }
