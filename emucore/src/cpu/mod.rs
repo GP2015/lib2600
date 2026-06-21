dcf757881c691b4561e4c716c7f563289fa3146f
GP2015 <adamcunningham2015@hotmail.com>
2026-06-21 15:42:37 +0100

    Began work on CPU instruction decoding

--- emucore/src/cpu/mod.rs
+++ emucore/src/cpu/mod.rs
@@ -1,15 +1,19 @@
+pub mod instructions;
 pub mod reads;
 pub mod regs;
 
 use crate::{
     common::{
+        HasCouldBe, HasMux,
         line::{
             multi::{BusDriveState, IsBusDriveState},
             single::DriveState,
         },
+        read::multi::IsMultiRead,
         signal::LineSignal,
     },
     cpu::{
+        instructions::Instruction,
         reads::{CpuAllReads, CpuLineReads},
         regs::CpuRegs,
     },
@@ -40,8 +44,70 @@ impl Cpu {
         todo!()
     }
 
+    fn update_s(&mut self, reads: &CpuAllReads) {
+        let def = &|| reads.reg.s;
+        let set_to_x = &|| reads.reg.x;
+        let dec = &|| reads.reg.s.decremented();
+        let inc = &|| reads.reg.s.incremented();
+
+        macro_rules! could_match {
+            ($cond:expr, $def:expr, ($opt:expr, $branch:expr) $(,)?) => {
+                HasMux::mux($cond.could_be(&$opt), $def, $branch)
+            };
+            (
+                $cond:expr,
+                $def:expr,
+                ($opt1:expr, $branch1:expr),
+                $(($other_opts:expr, $other_branches:expr)),+
+                $(,)?
+            ) => {
+                HasMux::mux(
+                    $cond.could_be(&$opt1),
+                    &|| could_match!($cond, $def, $(($other_opts, $other_branches))+),
+                    $branch1
+                )
+            };
+        }
+
+        self.reg.s = could_match!(
+            reads.reg.instr_cycle,
+            def,
+            (1, &|| could_match!(
+                reads.line.db,
+                def,
+                (Instruction::Txs, set_to_x)
+            )),
+            (2, &|| could_match!(
+                reads.line.db,
+                def,
+                (Instruction::Txs, set_to_x)
+            ))
+        );
+
+        // self.reg.s = could_match!(reads.reg.instr_cycle, def(), (1, set_to_x));
+
+        // self.reg.s = HasMux::mux(
+        //     reads.reg.instr_cycle.could_be(&1),
+        //     &|| HasMux::mux(reads.reg.instr_cycle.could_be(&2), def, dec),
+        //     set_to_x,
+        // );
+
+        // 2(1)
+        //     set <- TXS
+        // 3(2)
+        //     dec <- PHA/PHP/BRK
+        //     inc <- PLA/PLP/RTI/RTS
+        // 4(3)
+        //     dec <- BRK/JSR
+        //     inc <- RTI/RTS
+        // 5(4)
+        //     dec <- BRK/JSR
+        //     inc <- RTI
+    }
+
     pub fn handle_falling_edge(&mut self, line_reads: CpuLineReads) {
         let reads = CpuAllReads::new(line_reads, self.reg.clone());
+        self.update_s(&reads);
         todo!()
     }
 }
--- emucore/src/cpu/regs.rs
+++ emucore/src/cpu/regs.rs
@@ -1,8 +1,38 @@
+use crate::common::{BitReg, MBitReg};
+
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct CpuRegs {}
+pub struct CpuRegs {
+    pub instr_cycle: MBitReg<3>,
+    pub a: MBitReg<8>,
+    pub x: MBitReg<8>,
+    pub y: MBitReg<8>,
+    pub pc: MBitReg<16>,
+    pub s: MBitReg<8>,
+    pub n: BitReg,
+    pub v: BitReg,
+    pub b: BitReg,
+    pub d: BitReg,
+    pub i: BitReg,
+    pub z: BitReg,
+    pub c: BitReg,
+}
 
 impl CpuRegs {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            instr_cycle: [BitReg::Unknown; _],
+            a: [BitReg::Unknown; _],
+            x: [BitReg::Unknown; _],
+            y: [BitReg::Unknown; _],
+            pc: [BitReg::Unknown; _],
+            s: [BitReg::Unknown; _],
+            n: BitReg::Unknown,
+            v: BitReg::Unknown,
+            b: BitReg::Unknown,
+            d: BitReg::Unknown,
+            i: BitReg::Unknown,
+            z: BitReg::Unknown,
+            c: BitReg::Unknown,
+        }
     }
 }
